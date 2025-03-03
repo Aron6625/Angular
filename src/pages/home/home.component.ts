bb44a02fb5a23fbf56dea374251e35c939f2b11b
Aron <aronpad123@gmail.com>
2025-03-02 22:46:38 -0400

    Delete and change handler

--- src/pages/home/home.component.ts
+++ src/pages/home/home.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  lists = signal([
+      'fila1 ',
+      'fila2',
+      'fila 3',
+    ]);
+  changeHandler(event: Event){
+    const input = event.target as HTMLInputElement;
+    const newList = input.value;
+    this.lists.update((lists) => [...lists,newList]);
+    
+  }
+  deleteList(index: number){
+    this.lists.update((list) => list.filter((task, position) => position !== index));
+  }
 }
